257329500aac915e3ea21cce6c80df950482e877
jamsluo <jamsluo@tencent.com>
2023-04-07 15:40:13 +0800

    init doc retrieval

--- du_start.sh
+++ du_start.sh
@@ -167,6 +167,35 @@ python3 -m torch.distributed.launch --nproc_per_node 8 retrival_du.py \
   --pred_path $pred_path \
   --use_legacy_prediction_loop
 
+
+output_dir=/search/ai/jamsluo/passage_rank/du_task_output/infoxml_g3_5e5_64_512_multiple
+init_dir=/search/ai/pretrain_models/infoxlm-base/
+train_data_dir=/search/ai/jamsluo/passage_rank/DuReader-Retrieval-Baseline/formate_data/train/
+pred_path=/search/ai/jamsluo/passage_rank/DuReader-Retrieval-Baseline/formate_data/dev/dev.res.top10
+python3 -m torch.distributed.launch --nproc_per_node 8 retrival_du_mul.py \
+  --output_dir $output_dir \
+  --model_name_or_path  $init_dir \
+  --logging_steps 50 \
+  --do_train \
+  --save_steps 600 \
+  --train_dir $train_data_dir \
+  --q_max_len 64 \
+  --p_max_len 512 \
+  --seed 66 \
+  --per_device_train_batch_size 30 \
+  --train_group_size 3 \
+  --per_device_eval_batch_size 128 \
+  --warmup_ratio 0.1 \
+  --weight_decay 0.01 \
+  --learning_rate 5e-5 \
+  --num_train_epochs 30 \
+  --overwrite_output_dir \
+  --dataloader_num_workers 2 \
+  --evaluation_strategy steps \
+  --eval_steps 200 \
+  --pred_path $pred_path \
+  --use_legacy_prediction_loop
+
 output_dir=/search/ai/jamsluo/passage_rank/du_task_output/ernie_base_g2_5e5_dureader_train_sec
 init_dir=/search/ai/pretrain_models/models--nghuyong--ernie-3.0-base-zh/
 passage_path=/search/ai/jamsluo/passage_rank/DuReader-Retrieval-Baseline/formate_data/passage_idx.pkl
