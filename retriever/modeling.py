671ec564d682b23dd533eb5a74dfb80811415474
jamsluo <jamsluo@tencent.com>
2023-04-17 10:39:16 +0800

    init doc retrieval

--- retriever/modeling.py
+++ retriever/modeling.py
@@ -271,6 +271,69 @@ class RetrieverQA(nn.Module):
             loss = self.cross_entropy(scores, index)
             return loss, loss
 
+class RetrieverQAPooling(nn.Module):
+    def __init__(self, model: PreTrainedModel, model_args: ModelArguments, data_args: DataArguments,
+                 train_args: TrainingArguments, config):
+        super().__init__()
+        self.model: PreTrainedModel = model
+        self.cross_entropy = nn.CrossEntropyLoss(reduction='mean')
+        self.data_args, self.model_args, self.train_args = data_args, model_args, train_args
+
+    @classmethod
+    def from_pretrained(
+            cls, model_args: ModelArguments, data_args: DataArguments, train_args: TrainingArguments,
+            *args, **kwargs
+    ):
+        hf_model = AutoModel.from_pretrained(*args, **kwargs)
+        model = cls(hf_model, model_args, data_args, train_args, kwargs['config'])
+        return model
+
+    def save_pretrained(self, output_dir: str):
+        path = os.path.join(output_dir, "pytorch_model.bin")
+        model_to_save = self.module if hasattr(self, "module") else self
+        torch.save(model_to_save.state_dict(), path)
+        self.model.config.save_pretrained(output_dir)
+
+    def forward(self, qry_input: Dict, doc_input: Dict):
+        group_size = self.data_args.train_group_size
+        if 'label' in doc_input:
+            labels = doc_input.pop('label').to(self.model.device)
+        qry_input['input_ids'] = qry_input['input_ids'].to(self.model.device)
+        if 'token_type_ids' in qry_input:
+            qry_input['token_type_ids'] = qry_input['token_type_ids'].to(self.model.device)
+        qry_input['attention_mask'] = qry_input['attention_mask'].to(self.model.device)
+        doc_input['input_ids'] = doc_input['input_ids'].to(self.model.device)
+        if 'token_type_ids' in doc_input:
+            doc_input['token_type_ids'] = doc_input['token_type_ids'].to(self.model.device)
+        doc_input['attention_mask'] = doc_input['attention_mask'].to(self.model.device)
+        qry_out: BaseModelOutputWithPooling = self.model(**qry_input, return_dict=True)
+        doc_out: BaseModelOutputWithPooling = self.model(**doc_input, return_dict=True)
+
+        qry_token_embeddings =  qry_out.last_hidden_state  # First element of model_output contains all token embeddings
+        qry_input_mask_expanded = qry_input['attention_mask'].unsqueeze(-1).expand(qry_token_embeddings.size()).float()
+        qry_cls = torch.sum(qry_token_embeddings * qry_input_mask_expanded, 1) / torch.clamp(qry_input_mask_expanded.sum(1), min=1e-9)
+
+        doc_token_embeddings = doc_out.last_hidden_state
+        doc_input_mask_expanded = doc_input['attention_mask'].unsqueeze(-1).expand(doc_token_embeddings.size()).float()
+        doc_cls = torch.sum(doc_token_embeddings * doc_input_mask_expanded, 1) / torch.clamp(doc_input_mask_expanded.sum(1), min=1e-9)
+
+        if not self.training:
+            score_ir = torch.sum(qry_cls * doc_cls, dim=-1)
+            return score_ir
+        else:
+
+            scores = torch.matmul(qry_cls, doc_cls.transpose(1, 0))  # Q * D
+            index = torch.arange(
+                scores.size(0),
+                device=doc_input['input_ids'].device,
+                dtype=torch.long
+            )
+            # offset the labels
+            index = index * self.data_args.train_group_size
+            loss = self.cross_entropy(scores, index)
+            return loss, loss
+
+
 class RetrieverMean(nn.Module):
     def __init__(self, model: PreTrainedModel, model_args: ModelArguments, data_args: DataArguments,
                  train_args: TrainingArguments, config):
--- tools/triton_client_new.py
+++ tools/triton_client_new.py
@@ -3,44 +3,15 @@
 # pip3 install nvidia-pyindex
 # pip3 install tritonclient[all]
 import sys
-import time
-
 import numpy as np
 import tritonclient.http as httpclient
 from transformers import BertTokenizer, AutoTokenizer
 model_name = "embedding_mul_onnx"  # 模型目录名/venus注册模型名称
 address = "10.212.207.33:8000"  # 机器地址
 triton_client = httpclient.InferenceServerClient(url=address)
-rm_model_path = "/search/ai/jamsluo/passage_rank/du_task_output/ernie_base_g49_5e5/checkpoint-16000/"
-rm_model_path = "/search/ai/jamsluo/passage_rank/du_task_output/ernie_base_g2_5e5"
 rm_model_path = "/search/ai/pretrain_models/infoxlm-base/"
 tokenizer = AutoTokenizer.from_pretrained(rm_model_path, trust_remote_code=True)
 import torch
-
-def get_embedding_old(doc):
-    RM_input = tokenizer(doc, max_length=512, truncation=True, return_tensors="pt", padding=True)
-    # print(RM_input)
-    RM_batch = [torch.tensor(RM_input["input_ids"]).numpy(), torch.tensor(RM_input["token_type_ids"]).numpy(),  torch.tensor(RM_input["attention_mask"]).numpy()]
-
-    inputs = []
-    inputs.append(httpclient.InferInput('input_ids', list(RM_batch[0].shape), 'INT64'))
-    inputs.append(httpclient.InferInput('token_type_ids', list(RM_batch[1].shape), 'INT64'))
-    inputs.append(httpclient.InferInput('attention_mask', list(RM_batch[2].shape), 'INT64'))
-    inputs[0].set_data_from_numpy(RM_batch[0])
-    inputs[1].set_data_from_numpy(RM_batch[1])
-    inputs[2].set_data_from_numpy(RM_batch[2])
-    output = httpclient.InferRequestedOutput('output')
-    # try:
-    results = triton_client.infer(
-        model_name,
-        inputs,
-        model_version='1',
-        outputs=[output],
-        request_id='1'
-    )
-    results = results.as_numpy('output')
-    return results
-    # print(results, sep='\t')
 
 def get_embedding(doc):
     RM_input = tokenizer(doc, max_length=512, truncation=True, return_tensors="pt", padding=True)
@@ -63,76 +34,14 @@ def get_embedding(doc):
     )
     results = results.as_numpy('output')
     return results
-    # print(results, sep='\t')
 
-
-def break_sentence(text: str):
-    # 转换成小写
-    text = text.lower()
-    ret = list()
-
-    # 长度较短，不进行分句
-    if len(text) < HALF_SENT_LEN:
-        ret.append(text)
-        return ret
-
-    # 按照标点分句
-    last_pos = 0
-    for i, c in enumerate(text):
-        if c in break_punctations and i - last_pos > MIN_SENT_LEN:
-            segment = text[last_pos:i]
-            if len(segment) > MAX_SENT_LEN:
-                # 需要再次分句
-                subs = force_break_sentence(segment)
-                ret.extend(subs)
-            else:
-                ret.append(segment)
-
-            # 分隔符
-            last_pos = i + 1
-
-    # 结尾仍有残余部分需要分句
-    if last_pos < len(text):
-        segment = text[last_pos:]
-        if len(segment) > MAX_SENT_LEN:
-            subs = force_break_sentence(segment)
-            ret.extend(subs)
-        else:
-            ret.append(segment)
-
-    return ret
-
-break_punctations = {'！', '？', '!', '?', '。'}
-force_break_punctations = {',', ';', '，', '；'}
-MAX_SENT_LEN = 512
-HALF_SENT_LEN = 512
-MIN_SENT_LEN = 256
-# 按照小标点或长度强制分句
-def force_break_sentence(text: str):
-    ret = list()
-
-    # 已经满足要求
-    if len(text) <= MAX_SENT_LEN:
-        ret.append(text)
-        return ret
-
-    idx = MAX_SENT_LEN - 1
-    while idx >= 0 and text[idx] not in force_break_punctations:
-        idx -= 1
-
-    break_idx = idx + 1 if idx >= 0 else MAX_SENT_LEN
-    ret.append(text[:break_idx])
-
-    # 剩余部分
-    remains = text[break_idx:]
-    ret.extend(force_break_sentence(remains))
-    return ret
 
 if __name__ == '__main__':
     # step = docs.split('。')
     # parts = [step[i] for i in range(len(step))][:128]
     # docs = ' '.join(open(sys.argv[1]).readlines())
     # parts = break_sentence(docs)
+    torch.nn.Embedding
     docs = ' '.join(open(sys.argv[1]).readlines())
     step = len(docs) // 500
     parts = [docs[i*500:(i+1)*500] for i in range(step)]
