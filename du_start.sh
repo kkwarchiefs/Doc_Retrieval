0efcdf211d219a02d9c77d1057c70672ce1ab442
jamsluo <jamsluo@tencent.com>
2023-05-10 18:09:07 +0800

    init doc retrieval

--- du_start.sh
+++ du_start.sh
@@ -238,7 +238,7 @@ python3 -m torch.distributed.launch --nproc_per_node 8 retrival_squad_pooling.py
   --q_max_len 128 \
   --p_max_len 512 \
   --seed 66 \
-  --per_device_train_batch_size 48 \
+  --per_device_train_batch_size 44 \
   --train_group_size 2 \
   --per_device_eval_batch_size 128 \
   --warmup_ratio 0.1 \
@@ -249,6 +249,62 @@ python3 -m torch.distributed.launch --nproc_per_node 8 retrival_squad_pooling.py
   --dataloader_num_workers 2 \
   --evaluation_strategy steps \
   --save_steps 1000 \
+  --pred_path $pred_path \
+  --use_legacy_prediction_loop
+
+output_dir=/search/ai/jamsluo/passage_rank/du_task_output/passage_multi_openqa_squad_pooling_v2
+init_dir=/search/ai/pretrain_models/paraphrase-multilingual-mpnet-base-v2/
+train_data_dir=/search/ai/jamsluo/passage_rank/DuReader-Retrieval-Baseline/formate_data/train_squad_third/
+pred_path=/search/ai/jamsluo/passage_rank/DuReader-Retrieval-Baseline/formate_data/dev/dev_squad_pair_msmarco5k_ducross5k.tsv
+python3 -m torch.distributed.launch --nproc_per_node 8 retrival_squad_pooling.py \
+  --output_dir $output_dir \
+  --model_name_or_path  $init_dir \
+  --logging_steps 50 \
+  --do_train \
+  --save_steps 1000 \
+  --train_dir $train_data_dir \
+  --q_max_len 128 \
+  --p_max_len 512 \
+  --seed 66 \
+  --per_device_train_batch_size 44 \
+  --train_group_size 2 \
+  --per_device_eval_batch_size 128 \
+  --warmup_ratio 0.1 \
+  --weight_decay 0.01 \
+  --learning_rate 5e-5 \
+  --num_train_epochs 10 \
+  --overwrite_output_dir \
+  --dataloader_num_workers 2 \
+  --evaluation_strategy steps \
+  --eval_steps 1000 \
+  --pred_path $pred_path \
+  --use_legacy_prediction_loop
+
+
+output_dir=/search/ai/jamsluo/passage_rank/du_task_output/passage_multi_openqa_squad_pooling_exact
+init_dir=/search/ai/pretrain_models/paraphrase-multilingual-mpnet-base-v2/
+train_data_dir=/search/ai/jamsluo/passage_rank/DuReader-Retrieval-Baseline/formate_data/train_squad_msmarco_exact/
+pred_path=/search/ai/jamsluo/passage_rank/DuReader-Retrieval-Baseline/formate_data/dev/dev_squad_pair_msmarco_5k_exact.tsv
+python3 -m torch.distributed.launch --nproc_per_node 8 retrival_squad_pooling.py \
+  --output_dir $output_dir \
+  --model_name_or_path  $init_dir \
+  --logging_steps 50 \
+  --do_train \
+  --save_strategy epoch \
+  --train_dir $train_data_dir \
+  --q_max_len 128 \
+  --p_max_len 512 \
+  --seed 66 \
+  --per_device_train_batch_size 44 \
+  --train_group_size 2 \
+  --per_device_eval_batch_size 128 \
+  --warmup_ratio 0.1 \
+  --weight_decay 0.01 \
+  --learning_rate 5e-5 \
+  --num_train_epochs 4 \
+  --overwrite_output_dir \
+  --dataloader_num_workers 2 \
+  --evaluation_strategy epoch \
   --pred_path $pred_path \
   --use_legacy_prediction_loop
 
@@ -335,6 +391,9 @@ python3 -m torch.distributed.launch --nproc_per_node 8 retrival_squad_colbert.py
   --eval_steps 1000 \
   --pred_path $pred_path \
   --use_legacy_prediction_loop
+
+
+
 
 
 output_dir=/search/ai/jamsluo/passage_rank/du_task_output/rerank_mulit_mpnet_squad_v2
--- retrival_squad_pooling.py
+++ retrival_squad_pooling.py
@@ -194,6 +194,7 @@ def main():
         print(model_args)
         print(data_args)
         print(training_args)
+        print("train_dataset length", train_dataset.__len__())
         qry, doc = train_dataset[random.choice(range(train_dataset.__len__()))][:2]
         input_ids = qry['input_ids']
         print(' '.join(tokenizer.convert_ids_to_tokens(input_ids)))
@@ -201,6 +202,7 @@ def main():
         print(' '.join(tokenizer.convert_ids_to_tokens(input_ids)))
         print(qry, doc)
         print('eval datasets')
+        print("train_dataset length", eval_dataset.__len__())
         qry, doc = eval_dataset[random.choice(range(eval_dataset.__len__()))]
         input_ids = qry['input_ids']
         print(' '.join(tokenizer.convert_ids_to_tokens(input_ids)))
